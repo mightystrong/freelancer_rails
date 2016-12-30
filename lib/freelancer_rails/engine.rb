bcd2935da5b95102b2dd7924f6fc652b322de7bd
Michael Price <mike@mightystrongmedia.com>
2016-12-30 09:16:44 -0700

    Added basic file structure and engine file.

---
+++ lib/freelancer_rails/engine.rb
@@ -0,0 +1,13 @@
+module FreelancerRails
+  class Engine < ::Rails::Engine
+    initializer 'freelancer_rails.assets.precompile' do |app|
+
+      %w(stylesheets javascripts fonts images).each do |s|
+        app.config.assets.paths << root.join('app', 'assets', s).to_s
+        app.config.assets.paths << root.join('vendor', 'assets', s).to_s
+      end
+
+      # app.config.assets.precompile += %w( portfolio/**/*.jpg)
+    end
+  end
+end
